1750485779adeed7a3910c62bf29cf95123d938f
William Compton <will12@ksu.edu>
2019-09-18 17:24:39 -0500

    Triceritots

---
+++ Menu/Sides/Fryceritops.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Sides
+{
+
+    public class Fryceritops : Side
+    {
+
+    }
+    
+     
+
+
+}
---
+++ Menu/Sides/Triceritots.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Sides
+{
+
+    public class Triceritots : Side
+    {
+        private Size size;
+        public override Size Size
+        {
+            set
+            {
+                size = value;
+                switch (size)
+                {
+                    case Size.Small:
+                        Price = 0.99;
+                        Calories = 352;
+                        break;
+                    case Size.Medium:
+                        Price = 1.45;
+                        Calories = 410;
+                        break;
+                    case Size.Large:
+                        Price = 1.95;
+                        Calories = 590;
+                        break;
+                }
+                
+            }
+            get { return size; }
+
+        }
+
+        public Triceritots()
+        {
+            Price = 0.99;
+            Calories = 352;
+            Ingredients.Add("Potato");
+            Ingredients.Add("Salt");
+            Ingredients.Add("Vegetable Oil");
+        }
+
+    }
+}
