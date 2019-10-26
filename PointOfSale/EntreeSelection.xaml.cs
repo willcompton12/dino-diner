d1be88cf02baa9c19182bab8474b8ec2290911a2
William Compton <will12@ksu.edu>
2019-10-26 10:39:21 -0500

    Entree Selection Page

--- PointOfSale/EntreeSelection.xaml.cs
+++ PointOfSale/EntreeSelection.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DinoDiner.Menu;
 
 namespace PointOfSale
 {
@@ -20,12 +21,117 @@ namespace PointOfSale
     /// </summary>
     public partial class EntreeSelection : Page
     {
+        private Entree entree { get; set; }
         /// <summary>
         /// Creates a page to allow the user to select an entree
         /// </summary>
         public EntreeSelection()
         {
             InitializeComponent();
+        }
+        /// <summary>
+        /// Event handler to add Brontowurst to order
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public void OnBrontowurst(object sender, RoutedEventArgs args)
+        {
+            if (DataContext is Order order)
+            {
+                entree = new Brontowurst();
+                order.Add(entree);
+                NavigationService.Navigate(new MenuCategorySelection());
+            }
+
+        }
+        /// <summary>
+        /// Event handler to add Dino-Nuggets to the order
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public void OnDinoNuggets(object sender, RoutedEventArgs args)
+        {
+            if (DataContext is Order order)
+            {
+                entree = new DinoNuggets();
+                order.Add(entree);
+                NavigationService.Navigate(new MenuCategorySelection());
+            }
+        }
+        /// <summary>
+        /// Event handler to add Steakosaurus Burger to the order
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public void OnSteakosaurus(object sender, RoutedEventArgs args)
+        {
+            if (DataContext is Order order)
+            {
+                entree = new SteakosaurusBurger();
+                order.Add(entree);
+                NavigationService.Navigate(new MenuCategorySelection());
+            }
+
+        }
+        /// <summary>
+        /// Event handler to add Trex King burger to menu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public void OnTRex(object sender, RoutedEventArgs args)
+        {
+            if (DataContext is Order order)
+            {
+                entree = new TRexKingBurger();
+                order.Add(entree);
+                NavigationService.Navigate(new MenuCategorySelection());
+            }
+
+        }
+        /// <summary>
+        /// Event Handler to add Prehistoric PBJ to order
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public void OnPBJ(object sender, RoutedEventArgs args)
+        {
+            if (DataContext is Order order)
+            {
+                entree = new PrehistoricPBJ();
+                order.Add(entree);
+                NavigationService.Navigate(new MenuCategorySelection());
+            }
+
+        }
+        /// <summary>
+        /// Event Handler to add Pterodactyl Wings to order
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public void OnWings(object sender, RoutedEventArgs args)
+        {
+            if (DataContext is Order order)
+            {
+                entree = new PterodactylWings();
+                order.Add(entree);
+                NavigationService.Navigate(new MenuCategorySelection());
+            }
+
+        }
+        /// <summary>
+        /// Event handler to add Veloci-Wrap to order
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public void OnWrap(object sender, RoutedEventArgs args)
+        {
+            if (DataContext is Order order)
+            {
+                entree = new VelociWrap();
+                order.Add(entree);
+                NavigationService.Navigate(new MenuCategorySelection());
+            }
+
         }
     }
 }
--- PointOfSale/SideSelection.xaml.cs
+++ PointOfSale/SideSelection.xaml.cs
@@ -74,5 +74,6 @@ namespace PointOfSale
                side.Size = (DDSize)Enum.Parse(typeof(DinoDiner.Menu.Size), element.Tag.ToString()) ;
             }
         }
+
     }
 }
